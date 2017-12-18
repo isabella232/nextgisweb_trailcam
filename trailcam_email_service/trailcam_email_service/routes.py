12e3f11ae7b026c85a301788ddf7cf3d4959f4ce
Ivan Kavaliou <jownkiv@gmail.com>
2017-12-18 23:44:17 +0300

    #13 email routes were added to app

--- trailcam_email_service/trailcam_email_service/routes.py
+++ trailcam_email_service/trailcam_email_service/routes.py
@@ -1,3 +1,6 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('register_email', '/ngw/email/', request_method='POST')
+    config.add_route('unregister_email', '/ngw/email/', request_method='DELETE')
+    config.add_route('change_email', '/ngw/email/', request_method='PUT')
