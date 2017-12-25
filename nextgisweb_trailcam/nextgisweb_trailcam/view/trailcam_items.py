63cfd164d9061612c8a50b879688bf818dbd73d8
Ivan Kavaliou <jownkiv@gmail.com>
2017-12-25 22:23:01 +0300

    #12 get_trailcam_items was implemented

--- nextgisweb_trailcam/nextgisweb_trailcam/view/trailcam_items.py
+++ nextgisweb_trailcam/nextgisweb_trailcam/view/trailcam_items.py
@@ -3,6 +3,7 @@ from pyramid.httpexceptions import HTTPForbidden, HTTPNotFound
 
 from ..model import Trailcam
 from sqlalchemy.orm.exc import NoResultFound
+from nextgisweb.models import DBSession
 
 
 def trailcam_items_editor(request):
@@ -28,3 +29,17 @@ def trailcam_items_editor(request):
     return {
         'trailcam_detail': trailcam_dict
     }
+
+
+def get_trailcam_items(request):
+    trailcam_id = request.matchdict['trailcam_id']
+
+    try:
+        trailcam = DBSession.query(Trailcam).get(trailcam_id)
+        trailcam_items = trailcam.items
+    except NoResultFound:
+        raise HTTPNotFound()
+
+    trailcam_items_result = [trailcam_item.as_json_dict() for trailcam_item in trailcam_items]
+
+    return trailcam_items_result
